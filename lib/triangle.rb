ea34f7eac81f9887882c8cbd15f054ddf37a0dc0
fitzgebt <fitzgebt@gmail.com>
2021-01-27 17:04:59 -0800

    commit tree 7a25ad6b253189d85dd7e6ed619308c2ce330e17

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def illegal_length?
+    x = @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 ? true : false
+    y = @side_1 + @side_2 <= @side_3 || @side_3 + @side_2 <= @side_1 || @side_1 + @side_3 <= @side_2 ? true : false
+    x || y ? true : false
+  end
+
+  def kind
+    # binding.pry
+    if illegal_length?
+        raise TriangleError
+    elsif @side_1 == @side_2 && @side_1 == @side_3
+      return :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      return :isosceles
+    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
+      return :scalene
+    end
+
+  end
+  
+  class TriangleError < StandardError
+  end
+  
 end
